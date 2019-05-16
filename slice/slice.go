fd10b5e87bb29979ceb52f3e76c2a5390ee9bc76
sunqida <sunqida@foxmail.com>
2019-05-16 17:43:49 +0800

    fixed

--- slice/slice.go
+++ slice/slice.go
@@ -16,3 +16,37 @@ func RemoveRepeatedElement(arr []interface{}) (newArr []interface{}) {
 	}
 	return
 }
+
+func RemoveRepeatedInt(arr []int) (newArr []int) {
+	newArr = make([]int, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return
+}
+
+func RemoveRepeatedFloat64(arr []float64) (newArr []float64) {
+	newArr = make([]float64, 0)
+	for i := 0; i < len(arr); i++ {
+		repeat := false
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			newArr = append(newArr, arr[i])
+		}
+	}
+	return
+}
