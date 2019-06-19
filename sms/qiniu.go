0c3024b7d310c0cfd94d3cec504f21cf6c8d02ae
sunqida <sunqida@foxmail.com>
2019-06-19 09:25:54 +0800

    fixed

--- sms/qiniu.go
+++ sms/qiniu.go
@@ -21,7 +21,7 @@ func InitQiniuSMS(accessKey, secretKey string) {
 	auth := auth.New(accessKey, secretKey)
 	manager = sms.NewManager(auth)
 }
-func SendQiniuSMS(qiniuSMS QiniuSMS, mobile string) (code string, err error) {
+func SendQiniuSMS(qiniuSMS QiniuSMS, mobile string, code string) (err error) {
 	if !CheckRegexMobile(mobile) {
 		err = errors.New("手机号码不正确！")
 		return
@@ -32,8 +32,10 @@ func SendQiniuSMS(qiniuSMS QiniuSMS, mobile string) (code string, err error) {
 		return
 	}
 	RequestRegLimit--
-	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
-	code = fmt.Sprintf("%04d", rnd.Int31n(10000))
+	if code == "" {
+		rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+		code = fmt.Sprintf("%04d", rnd.Int31n(10000))
+	}
 	// SendMessage
 	args := sms.MessagesRequest{
 		SignatureID: qiniuSMS.SignatureID,
