e994670e45afea96e54c9cf7e6ccc8bbd0ef5a09
qida <sunqida@foxmail.com>
2019-11-14 10:30:47 +0800

    add

--- slice/slice.go
+++ slice/slice.go
@@ -50,3 +50,28 @@ func RemoveRepeatedFloat64(arr []float64) (newArr []float64) {
 	}
 	return
 }
+
+func RemoveElement(arr []interface{}, elem interface{}) []interface{} {
+	if len(arr) == 0 {
+		return arr
+	}
+	for i, v := range arr {
+		if v == elem {
+			arr = append(arr[:i], arr[i+1:]...)
+			return RemoveElement(arr, elem)
+		}
+	}
+	return arr
+}
+func RemoveElementInt(arr []int, elem int) []int {
+	if len(arr) == 0 {
+		return arr
+	}
+	for i, v := range arr {
+		if v == elem {
+			arr = append(arr[:i], arr[i+1:]...)
+			return RemoveElementInt(arr, elem)
+		}
+	}
+	return arr
+}
