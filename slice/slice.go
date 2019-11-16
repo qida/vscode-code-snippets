fc0835f4ccaf702feb0e7f0b687c7bf7c2a98458
qida <sunqida@foxmail.com>
2019-11-16 16:38:50 +0800

    去零值

--- slice/slice.go
+++ slice/slice.go
@@ -1,5 +1,10 @@
 package slice
 
+import (
+	"math"
+	"time"
+)
+
 func RemoveRepeatedElement(arr []interface{}) (newArr []interface{}) {
 	newArr = make([]interface{}, 0)
 	for i := 0; i < len(arr); i++ {
@@ -74,4 +79,48 @@ func RemoveElementInt(arr []int, elem int) []int {
 		}
 	}
 	return arr
+}
+
+func removeZero(slice []interface{}) []interface{} {
+	if len(slice) == 0 {
+		return slice
+	}
+	for i, v := range slice {
+		if IfZero(v) {
+			slice = append(slice[:i], slice[i+1:]...)
+			return removeZero(slice)
+		}
+	}
+	return slice
+}
+
+//判断一个值是否为零值，只支持string,float,int,time 以及其各自的指针，"%"和"%%"也属于零值范畴，场景是like语句
+func IfZero(arg interface{}) bool {
+	if arg == nil {
+		return true
+	}
+	switch v := arg.(type) {
+	case int, int32, int16, int64:
+		if v == 0 {
+			return true
+		}
+	case float32:
+		r := float64(v)
+		return math.Abs(r-0) < 0.0000001
+	case float64:
+		return math.Abs(v-0) < 0.0000001
+	case string:
+		if v == "" || v == "%%" || v == "%" {
+			return true
+		}
+	case *string, *int, *int64, *int32, *int16, *int8, *float32, *float64, *time.Time:
+		if v == nil {
+			return true
+		}
+	case time.Time:
+		return v.IsZero()
+	default:
+		return false
+	}
+	return false
 }
