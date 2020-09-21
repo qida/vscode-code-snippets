640299992479e376c1a98e80d1b9b40179e63a0a
qida <sunqida@foxmail.com>
2020-09-21 18:40:50 +0800

    map to struct

--- data/data.go
+++ data/data.go
@@ -6,6 +6,8 @@ import (
 	"crypto/cipher"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
+	"fmt"
 	"reflect"
 	"time"
 )
@@ -42,6 +44,53 @@ func ShortContent(content interface{}, force ...string) (m map[string]interface{
 	}
 	return
 }
+
+func SetField(obj interface{}, name string, value interface{}) error {
+	structValue := reflect.ValueOf(obj).Elem()
+	structFieldValue := structValue.FieldByName(name)
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("No such field: %s in obj", name)
+	}
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("Cannot set %s field value", name)
+	}
+	structFieldType := structFieldValue.Type()
+	val := reflect.ValueOf(value)
+	if structFieldType != val.Type() {
+		return errors.New("Provided value type didn't match obj field type")
+	}
+	structFieldValue.Set(val)
+	return nil
+}
+
+func CopyContent(master interface{}, second interface{}) (m map[string]interface{}) {
+	m = make(map[string]interface{})
+	var fields []string
+	t := reflect.TypeOf(master)
+	for i := 0; i < t.NumField(); i++ {
+		fields = append(fields, t.Field(i).Name)
+	}
+	val1 := reflect.ValueOf(master)
+	val2 := reflect.ValueOf(second)
+	var tt time.Time
+	for _, fname := range fields {
+		if val1.FieldByName(fname).Interface() == "" ||
+			val1.FieldByName(fname).Interface() == 0 ||
+			val1.FieldByName(fname).Interface() == nil ||
+			val1.FieldByName(fname).Interface() == uint(0) ||
+			val1.FieldByName(fname).Interface() == int8(0) ||
+			val1.FieldByName(fname).Interface() == int64(0) ||
+			val1.FieldByName(fname).Interface() == float64(0) ||
+			val1.FieldByName(fname).Interface() == tt {
+			m[fname] = val2.FieldByName(fname).Interface()
+		} else {
+			// fmt.Printf("%s:%+v\r\n", fname, val1.FieldByName(fname).Interface())
+			m[fname] = val1.FieldByName(fname).Interface()
+		}
+	}
+	return
+}
+
 func MD5V(str string) string {
 	h := md5.New()
 	h.Write([]byte(str))
